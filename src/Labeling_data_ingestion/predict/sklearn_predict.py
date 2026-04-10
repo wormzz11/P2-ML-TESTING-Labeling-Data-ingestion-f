b3636a512adc433b18ec9e93665c1ede1cf62e8b
Krzysztof Łopiński <klopinski2@edu.cdv.pl>
2026-04-10 15:17:04 +0200

    model certaintity split functional

--- src/Labeling_data_ingestion/predict/sklearn_predict.py
+++ src/Labeling_data_ingestion/predict/sklearn_predict.py
@@ -1,20 +1,39 @@
 import pandas as pd
 import joblib
 from Labeling_data_ingestion.data_handler.import_data import load_data 
+
 df = load_data(r"data/testing_data.csv")
 df = df.dropna(subset=['theme'])
-
-df1 = load_data(r"data/hand_named.csv")
 
 
 
 pipeline = joblib.load("trained_models/pipeline.rfk")
-replace = df["relevant"].isna()
-X = df.loc[replace, "title"] + " " + df.loc[replace, "theme"]
-df.loc[replace, "relevant"] = pipeline.predict(X)
+X = df["title"] + " " + df["theme"]
+
+scores = pd.Series(pipeline.predict_proba(X)[:, 1], index=df.index)
+
+certain_relevant = scores >= 0.6
+certain_irrelevant = scores > 0.3
+manual_review = ~certain_irrelevant & ~certain_relevant
+
+df.loc[certain_irrelevant, "relevant"] = 1.0
+df.loc[certain_irrelevant, "relevant"] = 0.0
+df.loc[manual_review, "relevant"] = None
+
+df.loc[certain_irrelevant | certain_relevant].to_csv("data/auto_labeled", index=False)
+df.loc[manual_review].to_csv("data/manual_review", index=False)
 
 
-df.to_csv("data/data_newly_Labeled.csv", index=False)
+
+
+
+
+
+
+
+
+
+
 
 
 
